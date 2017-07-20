6284a7a381cea3661ced0231bc2ef9f50091062d
jgibbons-cp <jgibbons@cloudpassage.com>
2017-07-20 14:30:28 -0700

    style cleanup

--- app/sva_scan_examples/config_helper.py
+++ app/sva_scan_examples/config_helper.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+
 class ConfigHelper():
     """
         Manage all configuration information for the application
@@ -13,13 +14,13 @@ class ConfigHelper():
         self.halo_key = os.getenv("HALO_API_KEY")
         self.halo_secret = os.getenv("HALO_API_SECRET_KEY")
 
-        scan_results_directory = os.environ["SCAN_RESULTS_DIRECTORY"] = "/tmp/scan_results/"
+        scan_results_directory = os.environ["SCAN_RESULTS_DIRECTORY"] = \
+            "/tmp/scan_results/"
         days_for_scan_age = os.environ["DAYS_FOR_SCAN_AGE"] = "0"
         scan_examples = os.environ["SCAN_EXAMPLES"] = "False"
 
         if self.halo_key is None or self.halo_secret is None \
             or not os.path.exists(scan_results_directory) \
-                or not isinstance( days_for_scan_age, int ) \
-                    or scan_examples != TRUE or scan_examples != FALSE:
-                        sys.exit(ERROR)
-
+                or not isinstance(days_for_scan_age, int) \
+                or scan_examples != TRUE or scan_examples != FALSE:
+                    sys.exit(ERROR)
