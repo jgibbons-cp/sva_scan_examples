533dd8391a0930f2d913365ac5b1c132ecd3d682
jgibbons-cp <jgibbons@cloudpassage.com>
2017-07-20 15:25:05 -0700

    unit testing

--- app/sva_scan_examples/config_helper.py
+++ app/sva_scan_examples/config_helper.py
@@ -10,25 +10,27 @@ class ConfigHelper():
         TRUE = "True"
         FALSE = "False"
         ERROR = 1
+        mode = "w"
 
         self.halo_key = os.getenv("HALO_API_KEY")
         self.halo_secret = os.getenv("HALO_API_SECRET_KEY")
 
         scan_results_directory = os.environ["SCAN_RESULTS_DIRECTORY"] = \
             "/tmp/scan_results/"
-        days_for_scan_age = os.environ["DAYS_FOR_SCAN_AGE"] = "0"
-        scan_examples = os.environ["SCAN_EXAMPLES"] = "False"
-
         path_exists = os.path.exists(scan_results_directory)
-        is_int = isinstance(days_for_scan_age, int)
 
-        print self.halo_key
-        print self.halo_secret
-        print is_int
-        print scan_examples
+        if not path_exists:
+            os.mkdir(scan_results_directory)
+            path_exists = os.path.exists(scan_results_directory)
+
+        days_for_scan_age = os.environ["DAYS_FOR_SCAN_AGE"] = "0"
+        days_for_scan_age = int(days_for_scan_age)
+        string_is_int_value = isinstance(days_for_scan_age, int)
+
+        scan_examples = os.environ["SCAN_EXAMPLES"] = "False"
 
         if self.halo_key is None or self.halo_secret is None \
             or not os.path.exists(scan_results_directory) or not path_exists \
-                or is_int == "False" \
+                or string_is_int_value == "False" \
                 or scan_examples != TRUE and scan_examples != FALSE:
                     sys.exit(ERROR)
