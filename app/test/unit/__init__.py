10aeb3d414fbc5ea5cd3cde37cc0387a86c2377a
jgibbons-cp <jgibbons@cloudpassage.com>
2017-08-02 23:22:52 -0700

    get halo_general

---
+++ app/test/unit/__init__.py
@@ -0,0 +1,24 @@
+from git import Repo
+import os
+import sys
+
+ERROR = 1
+
+# pull down the Halo API wrapper and manage module path
+halo_general_repo = "https://github.com/jgibbons-cp/halo_general.git"
+cwd = os.getcwd()
+repo_dir = "%s/halo_general" % cwd
+sys.path.append(repo_dir)
+
+# clone it if we don't have it
+if os.path.exists(repo_dir) is False:
+    Repo.clone_from(halo_general_repo, repo_dir)
+
+try:
+    from halo_general import HaloGeneral  # NOQA
+except ImportError as e:
+    print "Error: %s\n" % e
+    sys.exit(ERROR)
+
+__author__ = "CloudPassage"
+__version__ = "1.0"
