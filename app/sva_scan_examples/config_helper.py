1d012e08bd9fa3a1082b676318faf80346258aea
jgibbons-cp <jgibbons@cloudpassage.com>
2017-12-11 17:24:51 -0800

    flake8 fixes

--- app/sva_scan_examples/config_helper.py
+++ app/sva_scan_examples/config_helper.py
@@ -37,13 +37,11 @@ class ConfigHelper():
         heartbeat_interval = int(heartbeat_interval)
         hi_string_is_int_value = isinstance(heartbeat_interval, int)
 
-
         # for unit tests Travis populates the IP
         server_ip = "<server_ip>"
         os.environ["SERVER_IP"] = server_ip
 
-        unit_tests = os.environ["UNIT_TESTS"] = "no_unit_tests"
-
+        unit_tests = os.environ["UNIT_TESTS"] = "no_unit_tests" # NOQA
 
         if self.halo_key is None or self.halo_secret is None \
             or not os.path.exists(scan_results_directory) or not path_exists \
